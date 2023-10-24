8e30bead9ecd1e5bff3038262bbd991cfb86b15b
eduardo <eduardo.rodriguez.info@gmail.com>
2023-10-24 11:11:05 -0400

    Proyect Create Management Service Scrum

---
+++ ManagementServiceScrum/src/main/java/com/solutioncode/scrum/ManagementScrumApplication.java
@@ -0,0 +1,13 @@
+package com.solutioncode.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementScrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementScrumApplication.class, args);
+	}
+
+}
